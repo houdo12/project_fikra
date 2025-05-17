5ad644d63a5b5191f8a0ca7378b8cbc6480bfb48
houdo12 <houda.benhassoun@e-polytechnique.ma>
2025-05-17 23:08:36 +0200

    register_screen.dart

---
+++ register_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Créer un compte")),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: "Mot de passe"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text("S'inscrire"),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/login');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
